c364288dec8c590364d503c0fb73ac6663490e0e
Michael Rose <michael_rose@outlook.com>
2016-10-18 22:18:12 -0700

    Add chunk function

---
+++ __tests__/chunk.js
@@ -0,0 +1,12 @@
+const fp = require('fp');
+
+describe('chunk', () => {
+  it('returns a function', () => {
+    expect(fp.chunk()).toBeInstanceOf(Function);
+  });
+  it('breaks the array into correctly-sized chunks', () => {
+    expect(fp.chunk(1)([1,2,3])).toEqual([[1],[2],[3]]);
+    expect(fp.chunk(2)([1,2,3])).toEqual([[1,2],[3]]);
+    expect(fp.chunk(3)([1,2,3])).toEqual([[1,2,3]]);
+  })
+});
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const fp = {};
 
+fp.chunk = (size) => {
+  return (arr) => {
+    const chunks = [];
+    for(let i = 0; i < arr.length; i += size) {
+      chunks.push(arr.slice(i, i + size));
+    }
+    return chunks;
+  };
+};
+
 fp.each = (iteratee) => {
   return (arr) => {
     for(let i = 0; i < arr.length; i++) {
